834fd63e37435de5e7190d372328b5e4f1778339
atakurt034 <69969985+atakurt034@users.noreply.github.com>
2020-12-27 14:01:10 +0800

    ready for deploy

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import morgan from 'morgan'
+import path from 'path'
 
 import users from './routes/userRoutes.js'
 import projects from './routes/projectRoutes.js'
@@ -17,6 +18,20 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
   app.use(morgan('dev'))
+}
+
+const __dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
 }
 
 app.use('/api/users', users)
